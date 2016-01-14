698c4ad2c97f9a6be60f278ef09c08bf2427c4ff
Armani Hall <MRBOJANGLES226@GMAIL.COM>
2016-01-14 17:00:13 -0500

    iframe working but timer start resetting page

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
-  // $("#recipeSearch").on("click", function(){
   var ingredientSearch = $('#ingredients');
   var ingredientQuery = "apple"//ingredientSearch.val();
-  // var calorieSearch = $('#calories');
-  // var calorieQuery = calorieSearch.val();
 
   var url = "https://api.edamam.com/search?q="+ingredientQuery + "&id=ec41a8fc&key=6b6789b4bd25173849ec364d5810f34f.json"
   $.ajax({
@@ -11,50 +8,40 @@ $(document).ready(function(){
     type: "get",
     dataType: "jsonp"
   }).done(function(response){
-      console.log(response['hits'])
-      var hits = response['hits']
-      for (var i = 0; i < hits.length; i++) {
-        var hit = hits[i]
-        addToRecipeList(hit.recipe)
-        event.preventDefault();
-      }
-      // alert(response['hits'])
-    }).fail(function(){
-      console.log("Ajax request fails!")
-    }).always(function(){
-      console.log("This always happens regardless of successful ajax request or not.")
-    });
+    console.log(response['hits'])
+    var hits = response['hits']
+    for (var i = 0; i < hits.length; i++) {
+      var hit = hits[i]
+      addToRecipeList(hit.recipe)
+      event.preventDefault();
+    }
+  }).fail(function(){
+    console.log("Ajax request fails!")
+  }).always(function(){
+    console.log("This always happens regardless of successful ajax request or not.")
+  });
   // });
-});
-  // var i = 1;
-  function addToRecipeList(recipe){
-  var $recipe = $('<a href="' +recipe.url +'"> </a>')
-    $recipe.append('<h2>' + recipe.label + '</h2>')
-    $recipe.append('<p>' + recipe.source + '</p>')
-    $recipe.append('<img src="'+recipe.image+'"/>')
-    $('.jsrecipes').append($recipe)
-      $(".jsrecipes").each(function(i) {
 
-    $(".jsrecipes").on('click', function(event) {
-       href = $(".jsrecipes").attr('src');
-       $('iframe').show('slow')// $('#recipe_url').append(recipe.url)
-       $('#iframe').attr('src', recipe.url)
-        event.preventDefault();
-       alert(href);
-        return false;
-});
-})
-    // i++
+function addToRecipeList(recipe){
+  var $recipe = $('<a href="' +recipe.url +'"> </a>')
+  $recipe.append('<h2>' + recipe.label + '</h2>')
+  $recipe.append('<p>' + recipe.source + '</p>')
+  $recipe.append('<img src="'+recipe.image+'"/>')
+  $('.jsrecipes').append($recipe)
+    $("a").click(function(event) {
+      $('iframe').show('slow').attr('src', $(this).attr('href'));// $('#recipe_url').append(recipe.url)
+      event.preventDefault();
+    });
+  }
+
 
   $('#closeIframe').on('click', function(){
     $('iframe').hide('fast')
-    // return false;
   });
   // function breakout() {
   //   if (window.top != window.location) {
   //       window.top.location = "";
-    // }
-    //
-    // }
-// });
-};
+  // }
+  //
+  // }
+});
--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   var i = 1
   $('#addTimer').click(function(i){
-    $('#timerContainer:last').after('<button id="removeTimer">Remove Timer</button><div class="timerContainer"> <div class="title"> <p></p> </div> <div class="clock"> <p>0:00</p> </div> <input id="title" type="text" placeholder="Enter item here"> <input id="request" type="text" placeholder="Enter an amount here"> <a href="#" class="click">Click here</a> </div>');
+    $('#timerContainer').after('<div id="timerContainer"> <button id="removeTimer">Remove Timer</button> <div class="title"> <p></p> </div> <div class="clock"> <p>0:00</p> </div> <input id="title" type="text" placeholder="Enter item here"> <input id="request" type="text" placeholder="Enter an amount here"> <a href="#" class="click">Click here</a> </div>')//.attr('id', "" +i);
     // $("#timerContainer").each(function(event, i) {
-      $(this).attr('id', "timer" + (i + 1));
     // });
     // event.preventDefault
+    i++
   });
   $('#timerDiv').on('click','#removeTimer', function(){
     $(this).parent().remove();
@@ -23,13 +23,22 @@ $(document).ready(function(){
           totalAmount--;
           if (totalAmount == 0) {
               clearInterval(loop);
-          }
+          },
+          // pause: function(){
+          //   clearInterval(this.interval);
+          //   delete this.interval;
+          // },
+          // resume: function(){
+          //   if (!this.interval) this.start()
+          // }
+
+          // var hours = parseInt(totalAmount/3600);
           var minutes = parseInt(totalAmount/60);
           var seconds = parseInt(totalAmount%60);
           var title = inputTitle;
           if(seconds < 10)
               seconds = "0"+seconds;
-          $('.clock').text(minutes + ":" + seconds);
+          $('.clock').text(hours + ":" + minutes + ":" + seconds);
           $('.title').text(title);
       };
 
